ea98ca110d0dad7f28062caea9bfcea527ce40ff
Jacob Kim <jpoly1219@gmail.com>
2020-02-24 20:05:41 -0500

    redundancy.py now pipes 'ps -ef' to grep 'maincontrol.py'

--- redundancy.py
+++ redundancy.py
@@ -9,7 +9,7 @@ import time
 
 while True:
     p1 = subprocess.Popen(["ps", "-ef"], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["grep", "maincontrol.py"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    p2 = subprocess.Popen(["grep", "[m]aincontrol.py"], stdin=p1.stdout, stdout=subprocess.PIPE)
     p1.stdout.close()
     output = p2.communicate()[0]
     print(output)
